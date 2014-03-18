9dad367d0c104bbb49fecd37a126345ea6a31684
chenkongliang <chenkongliang@tpxip.com>
2014-03-18 15:18:56 +0800

    add mount-win7.sh #	new file:   shell/mount-win7.sh

---
+++ shell/mount-win7.sh
@@ -0,0 +1,2 @@
+mount -t cifs -o username=Administrator,password=xxxx,dir_mode=0777,file_mode=0777  //192.168.1.102/download ~/win7-download
+mount -t cifs -o username=Administrator,password=xxxx,dir_mode=0777,file_mode=0777 //192.168.1.102/e ~/e
